af86baecd3d69d3b296b4af89938a87da8f2b530
intent-ly <erick.skillsusa@gmail.com>
2020-10-13 15:55:47 -0700

    version1'

---
+++ team-builder/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Forum from './Form.js'
+
+
+function App() {
+  return (
+    
+    <div className="App">
+      <Forum />
+    </div>
+    
+  );
+}
+
+export default App;
